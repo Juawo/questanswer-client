fd86661dc745a3748dc121fe6d682ac036eff0b3
Juawo <joaopedrojp2703@gmail.com>
2026-04-29 11:00:40 -0300

    refactor: Remove unused scene and script files to improve project organization

--- scripts/teste.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-var card_scene: PackedScene = preload("res://scenes/card.tscn")
-
-func _ready() -> void:
-	var instance = card_scene.instantiate()
-	print("intance : ", instance)
