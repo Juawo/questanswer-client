3241ce311f9d65d79fca4a07302cbd73a93a5aa3
Juawo <joaopedrojp2703@gmail.com>
2026-05-20 12:34:18 -0300

    feat: enhance main menu scene with fade-in and fade-out transitions

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -1,10 +1,20 @@
 extends Control
 
 func _on_play_btn_pressed() -> void:
+	await SceneTrasition.fade_in(1)
+	if SessionState.cards_from_database.is_empty() :
+		ApiManager.fetch_unplayed_cards()
+		await ApiManager.cards_fetched_sucessfully
+	else :
+		await get_tree().create_timer(0.3).timeout
 	get_tree().change_scene_to_file("res://scenes/select_card/select_card.tscn")
-
+	SceneTrasition.fade_out(1)
+	
 func _on_credits_btn_pressed() -> void:
+	await SceneTrasition.fade_in(0.8)
 	get_tree().change_scene_to_file("res://scenes/credits/credits.tscn")
+	SceneTrasition.fade_out(0.8)
+	
 
 func _on_exit_btn_pressed() -> void:
 	get_tree().quit()
